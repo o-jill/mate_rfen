f0c1a75de8b8084f700b103de23c66521fcb4274
o-jill <nobaokix@gmail.com>
2026-07-09 16:10:36 +0900

    fixed writing merged result.

--- tools/usu.rb
+++ tools/usu.rb
@@ -20,12 +20,36 @@ end
 WINDOWS_NEWLINE = "\n"
 
 def read_lines(path)
+  puts "reading #{path} ..."
   File.readlines(path, chomp: true, encoding: "UTF-8")
 end
 
+# 行を連結して出力しようとすると
+# 扱えるバッファサイズを超えるので
+# 少しずつ出力する
 def write_utf8_no_bom(path, lines)
+  File.delete(path) if File.exist?(path)
+
+  # 10000行ずつファイルに出力
+  lines.each_slice(10000) do |l|
+    content = l.join(WINDOWS_NEWLINE)
+    content += WINDOWS_NEWLINE
+
+    File.open(path, "ab") do |f|
+      f.write(content.encode("UTF-8"))
+    end
+  end
+
+  # content = lines.join(WINDOWS_NEWLINE)
+  # content += WINDOWS_NEWLINE unless lines.empty?
+  # File.binwrite(path, content.encode("UTF-8"))
+end
+
+def write_utf8_no_bom_ikki(path, lines)
+  # 全部結合
   content = lines.join(WINDOWS_NEWLINE)
   content += WINDOWS_NEWLINE unless lines.empty?
+  # ファイルに出力
   File.binwrite(path, content.encode("UTF-8"))
 end
 
@@ -33,7 +57,7 @@ start_time = Time.now
 
 merged_lines = read_lines(input_path1) + read_lines(input_path2)
 
-write_utf8_no_bom(output_path, merged_lines)
+# write_utf8_no_bom(output_path, merged_lines)
 puts "merged."
 
 unique_sorted_lines = merged_lines.sort.uniq
