82b8860e3bd18c4457ac5171058e515c6ef8644c
o-jill <nobaokix@gmail.com>
2026-08-07 11:15:25 +0900

    added adaptive probability drawing.

--- tools/lottery_mate.rb
+++ tools/lottery_mate.rb
@@ -1,13 +1,13 @@
 #!/usr/bin/env ruby
 #
 # usage:
-# ruby script.rb <path> <100.0%> <path>
+# ruby lottery_mate.rb <path> <100.0%> <path>
 #
 # example:
-# ruby script.rb sample.txt 3.14 output.txt
+# ruby lottery_mate.rb sample.txt 3.14 output.txt
 
 if ARGV.length != 3
-  puts "Usage: ruby #{$0} <input file_path> <probability_percent> <output file_path>"
+  warn "Usage: ruby #{$0} <input file_path> <probability_percent> <output file_path>"
   exit 1
 end
 
@@ -16,12 +16,36 @@ probability = ARGV[1].to_f / 100.0
 output_path = ARGV[2]
 
 unless File.exist?(input_path)
-  puts "File not found: #{input_path}"
+  warn "File not found: #{input_path}"
   exit 1
 end
 
-File.open(output_path, "w") do |out|
-  File.foreach(input_path) do |line|
-    out.print line if rand < probability
+# ファイルからprobabilityで設定された確率で行を抽出してファイルに出力
+def draw_lines
+  File.open(output_path, "wb") do |out|
+    File.foreach(input_path) do |line|
+      out.print line if rand < probability
+    end
   end
 end
+
+# ファイルからprobabilityで設定された確率と評価値から
+# 抽出確率を決めて行を抽出してファイルに出力
+# 差が開いた棋譜が多め
+# 0->0.1, 1->1.1, 2->log2(3)+0.1, 64->log2(65)+0.1
+def draw_lines_depth_adaptive
+  File.open(output_path, "wb") do |out|
+    File.foreach(input_path) do |line|
+      m = /,-?([0-9.]+)/.match(line)
+      continue unless m  # 末尾に評価値がない
+
+      # 0->0.1, 1->1.1, 2->log2(3)+0.1, 64->log2(65)+0.1
+      corrected_probability = probability / (Math.log2(m[1].to_f + 1) + 0.1)
+
+      out.print line if rand < corrected_probability
+    end
+  end
+end
+
+draw_lines()
+# draw_lines_depth_adaptive()
